5fa18a4ad85dfec94247a476061601a16f3a841f
Stander-by <2472005423@qq.com>
2024-03-17 18:57:14 +0800

    update config.py

---
+++ config.py
@@ -0,0 +1,12 @@
+# coding:UTF-8
+__author__ = 'lee'
+
+DEBUG = True
+
+WTF_CSRF_ENABLED = False
+
+SECRET_KEY = '!@#$%8F6F98EC3684AECA1DC44E1CB816E4A5^&*()'
+
+UPLOAD_FOLDER = './pcaps/upload/'
+FILE_FOLDER = './pcaps/files/'
+PDF_FOLDER = './pcaps/pdf/'
