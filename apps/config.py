a9daa6fc5698d3f6e69cfa847853cea234d8af77
Stander-by <2472005423@qq.com>
2024-04-05 23:41:44 +0800

    update config.py

--- apps/config.py
+++ apps/config.py
@@ -5,8 +5,8 @@ Copyright (c) 2019 - present AppSeed.us
 
 import os
 
-class Config(object):
 
+class Config(object):
     basedir = os.path.abspath(os.path.dirname(__file__))
 
     # Set up the App SECRET_KEY
@@ -15,19 +15,20 @@ class Config(object):
 
     # This will create a file in <app> FOLDER
     SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'db.sqlite3')
-    SQLALCHEMY_TRACK_MODIFICATIONS = False 
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
     # Assets Management
-    ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')    
-    
-    SOCIAL_AUTH_GITHUB  = False
+    ASSETS_ROOT = os.getenv('ASSETS_ROOT', '/static/assets')
 
-    GITHUB_ID      = os.getenv('GITHUB_ID')
-    GITHUB_SECRET  = os.getenv('GITHUB_SECRET')
+    SOCIAL_AUTH_GITHUB = False
+
+    GITHUB_ID = os.getenv('GITHUB_ID')
+    GITHUB_SECRET = os.getenv('GITHUB_SECRET')
 
     # Enable/Disable Github Social Login    
     if GITHUB_ID and GITHUB_SECRET:
-         SOCIAL_AUTH_GITHUB  = True
+        SOCIAL_AUTH_GITHUB = True
+
 
 class ProductionConfig(Config):
     DEBUG = False
@@ -39,12 +40,12 @@ class ProductionConfig(Config):
 
     # PostgreSQL database
     SQLALCHEMY_DATABASE_URI = '{}://{}:{}@{}:{}/{}'.format(
-        os.getenv('DB_ENGINE'   , 'mysql'),
-        os.getenv('DB_USERNAME' , 'appseed_db_usr'),
-        os.getenv('DB_PASS'     , 'pass'),
-        os.getenv('DB_HOST'     , 'localhost'),
-        os.getenv('DB_PORT'     , 3306),
-        os.getenv('DB_NAME'     , 'appseed_db')
+        os.getenv('DB_ENGINE', 'mysql'),
+        os.getenv('DB_USERNAME', 'appseed_db_usr'),
+        os.getenv('DB_PASS', 'pass'),
+        os.getenv('DB_HOST', 'localhost'),
+        os.getenv('DB_PORT', 3306),
+        os.getenv('DB_NAME', 'appseed_db')
     )
 
 
@@ -55,5 +56,6 @@ class DebugConfig(Config):
 # Load all possible configurations
 config_dict = {
     'Production': ProductionConfig,
-    'Debug'     : DebugConfig
+    'Debug': DebugConfig,
+
 }
